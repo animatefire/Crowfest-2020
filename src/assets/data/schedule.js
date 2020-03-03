068b9aae315943becc4f8fd4980a1b0ad7ca64b2
John E. Pfannkuchen <johnpfannkuchen.webdev@gmail.com>
2020-03-03 15:45:35 -0500

    -Redownloaded all band images -inlined band images through schedule object

--- src/assets/data/schedule.js
+++ src/assets/data/schedule.js
@@ -1,164 +1,183 @@
+import fireSide from '../img/fire_side.jpg';
+import fireSide2 from '../img/fire_side_2.jpg';
+import eagles from '../img/artists/eagles.jpg';
+import kevinDoupe from '../img/artists/kevin_doupe.jpg';
+import uncleUku from '../img/artists/uncle_uku.jpg';
+import timRuffo from '../img/artists/tim_ruffo.jpeg';
+import nextToKin from '../img/artists/next_to_kin.jpg';
+import smokinCrows from '../img/artists/smokin_crows.jpeg';
+import glenmarysGhost from '../img/artists/glenmarys_ghost.jpg';
+import partOfPluto from '../img/artists/part_of_pluto.jpg';
+import jFloyd from '../img/artists/j_floyd.jpg';
+import geneHummel from '../img/artists/gene_hummel.jpg';
+import mikeDavis from '../img/artists/mike_davis.jpg';
+import tijuanaDangerDogs from '../img/artists/tijuana_danger_dogs.jpg';
+import underTheRug from '../img/artists/under_the_rug.jpg';
+import woodshedProphets from '../img/artists/woodshed_prophets.jpg';
+import randyMcstine from '../img/artists/randy_mcstine.jpg';
+import bootknife from '../img/artists/bootknife.jpg';
+
 const schedule = [
   {
     day: 'FRIDAY',
     time: '5:30-6:00',
     artist: 'Kevin Doupe',
     stage: 'Side Stage',
-    img: 'kevin_doupe.jpg'
+    img: kevinDoupe
   },
   {
     day: 'FRIDAY',
     time: '6:00-7:00',
     artist: 'Uncle Uku and The Guise',
     stage: 'Main Stage',
-    img: 'uncle_uku.jpg'
+    img: uncleUku
   },
   {
     day: 'FRIDAY',
     time: '7:00-7:30',
     artist: 'Tim Ruffo [with special guest Michaela Clark]',
     stage: 'Side Stage',
-    img: 'tim_ruffo.jpeg'
+    img: timRuffo
   },
   {
     day: 'FRIDAY',
     time: '7:30-8:30',
     artist: "The Smokin' Crows",
     stage: 'Main Stage',
-    img: 'smokin_crows.jpeg'
+    img: smokinCrows
   },
   {
     day: 'FRIDAY',
     time: '8:30-9:00',
     artist: 'Marty ',
     stage: 'Side Stage',
-    img: 'fireside.jpg'
+    img: fireSide2
   },
   {
     day: 'FRIDAY',
     time: '9:00-10:30',
     artist: "Eagle's Tribute",
     stage: 'Main Stage',
-    img: 'eagles.jpg'
+    img: eagles
   },
   {
     day: 'FRIDAY',
     time: '10:30',
     artist: 'Fireside Jam',
     stage: 'Fireside',
-    img: 'fireside.jpg'
+    img: fireSide
   },
   {
     day: 'SATURDAY',
     time: '12:00-1:00',
     artist: "Glenmary's Ghost",
     stage: 'Main stage',
-    img: 'glenmarys_ghost.jpg'
+    img: glenmarysGhost
   },
   {
     day: 'SATURDAY',
     time: '1:00-1:15',
     artist: 'George Gleason',
     stage: 'Side stage',
-    img: 'fireside.jpg'
+    img: fireSide2
   },
   {
     day: 'SATURDAY',
     time: '1:15-2:15',
     artist: 'Next to Kin',
     stage: 'Main stage',
-    img: 'next_to_kin.jpg'
+    img: nextToKin
   },
   {
     day: 'SATURDAY',
     time: '2:15-2:30',
     artist: 'Nick Ruffo',
     stage: 'Side stage',
-    img: 'fireside.jpg'
+    img: fireSide2
   },
   {
     day: 'SATURDAY',
     time: '2:30-3:30',
     artist: 'A Part of Pluto',
     stage: 'Main stage',
-    img: 'part_of_pluto.jpg'
+    img: partOfPluto
   },
   {
     day: 'SATURDAY',
     time: '3:30-3:45',
     artist: 'Chris Ruffo',
     stage: 'Side stage',
-    img: 'fireside.jpg'
+    img: fireSide2
   },
   {
     day: 'SATURDAY',
     time: '3:45-4:45',
     artist: 'J Floyd & The Grave Sitters',
     stage: 'Main stage',
-    img: 'j_floyd.jpg'
+    img: jFloyd
   },
   {
     day: 'SATURDAY',
     time: '4:45-5:00',
     artist: 'Gene Hummel',
     stage: 'Side stage',
-    img: 'gene_hummel.jpg'
+    img: geneHummel
   },
   {
     day: 'SATURDAY',
     time: '5:00-6:00',
     artist: 'Mike Davis and LBE',
     stage: 'Main stage',
-    img: 'mike_davis.jpeg'
+    img: mikeDavis
   },
   {
     day: 'SATURDAY',
     time: '6:00-6:15',
     artist: 'Tim Ruffo',
     stage: 'Side stage',
-    img: 'tim_ruffo.jpeg'
+    img: fireSide2
   },
   {
     day: 'SATURDAY',
     time: '6:15-7:15',
     artist: 'Woodshed Prophets',
     stage: 'Main stage',
-    img: 'woodshed_prophets.jpg'
+    img: woodshedProphets
   },
   {
     day: 'SATURDAY',
     time: '7:15-7:45',
     artist: 'Under the Rug',
     stage: 'Side stage',
-    img: 'under_the_rug.jpg'
+    img: underTheRug
   },
   {
     day: 'SATURDAY',
     time: '7:45-8:45',
     artist: 'Tijuana Danger Dogs',
     stage: 'Main stage',
-    img: 'tijuana_danger_dogs.jpg'
+    img: tijuanaDangerDogs
   },
   {
     day: 'SATURDAY',
     time: '8:45-9:15',
     artist: 'Bootknife',
     stage: 'Side stage',
-    img: 'bootknife.jpg'
+    img: bootknife
   },
   {
     day: 'SATURDAY',
     time: '9:15-10:30',
     artist: 'Randy McStine Trio',
     stage: 'Main stage',
-    img: 'randy_mcstine.jpg'
+    img: randyMcstine
   },
   {
     day: 'SATURDAY',
     time: '10:30',
     artist: 'Fireside Jam',
     stage: 'Fireside',
-    img: 'fireside.jpg'
+    img: fireSide
   }
 ];
 
--- src/components/artistCard.vue
+++ src/components/artistCard.vue
@@ -5,15 +5,15 @@
       :id="`${schedule[i].img}`"
       :class="artist"
       :key="`artist-${i}`"
-      :style="{ backgroundImage: `url('img/artists/${schedule[i].img}')`}"
+      :style="{ backgroundImage: `url('${schedule[i].img}')` }"
     >
       <div class="screen">
         <div class="info">
-          <h2>{{schedule[i].artist}}</h2>
-          <h4>{{schedule[i].day}}</h4>
+          <h2>{{ schedule[i].artist }}</h2>
+          <h4>{{ schedule[i].day }}</h4>
           <p>
-            <strong>{{schedule[i].time}}</strong>
-            <em>{{schedule[i].stage}}</em>
+            <strong>{{ schedule[i].time }}</strong>
+            <em>{{ schedule[i].stage }}</em>
           </p>
         </div>
       </div>
@@ -22,42 +22,42 @@
 </template>
 
 <script>
-import schedule from "../assets/data/schedule.js";
-export default {
-  data() {
-    return {
-      schedule
-    };
-  }
-};
+  import schedule from '../assets/data/schedule.js';
+  export default {
+    data() {
+      return {
+        schedule
+      };
+    }
+  };
 </script>
 
 <style scoped lang="scss">
-h1,
-h2,
-h3,
-h4,
-h5,
-p {
-  color: hsl(20, 20, 85);
-}
-.info {
-  position: absolute;
-  top: 50%;
-  left: 40%;
-  transform: translate(-50%, -50%);
-}
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  p {
+    color: hsl(20, 20, 85);
+  }
+  .info {
+    position: absolute;
+    top: 50%;
+    left: 40%;
+    transform: translate(-50%, -50%);
+  }
 
-.screen {
-  background-color: rgba(0, 0, 0, 0.4);
-  height: 100%;
-  padding: 1em;
-}
+  .screen {
+    background-color: rgba(0, 0, 0, 0.4);
+    height: 100%;
+    padding: 1em;
+  }
 
-.artist {
-  background-repeat: no-repeat;
-  background-size: cover;
-  height: 75vh;
-  position: relative;
-}
+  .artist {
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 75vh;
+    position: relative;
+  }
 </style>
