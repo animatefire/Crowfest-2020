07673ce744157a2aa3d74a88f1820d24e7295c50
John E. Pfannkuchen <johnpfannkuchen.webdev@gmail.com>
2020-03-17 18:22:22 -0400

    Reverted artistCard styles...

--- src/components/artistCard.vue
+++ src/components/artistCard.vue
@@ -1,57 +1,24 @@
 <template>
-  <div>
-    <div
-      v-for="(artist, i) in schedule"
-      @mouseover="hover = i"
-      :class="artist"
-      :key="`artist-${i}`"
-      :alt="artist"
-    >
-      <p>
-        <span class="schedule-artist">{{ artist.artist }}</span>
-        <br />
-        <span class="schedule-day">{{ artist.day }}</span>/
-        <span class="schedule-time">{{ artist.time }}</span>/
-        <span class="schedule-stage">{{ artist.stage }}</span>
-      </p>
+  <div id="background" :style="{ backgroundImage: `url('${schedule[hover].img}')`}">
+    <div class="screen">
+      <logo class="poster-header" />
 
-      <div
-        class="members-and-bio"
-        v-if="schedule[hover].members && schedule[hover].members == schedule[i].members"
-      >
-        <a :href="artist.website" target="_blank">
-          <website-icon v-if="artist.website" class="icon" />
-        </a>
-        <a :href="artist.twitter" target="_blank">
-          <twitter-icon v-if="artist.twitter" class="icon" />
-        </a>
-        <a :href="artist.facebook" target="_blank">
-          <facebook-icon v-if="artist.facebook" class="icon" />
-        </a>
-        <a :href="artist.instagram" target="_blank">
-          <instagram-icon v-if="artist.instagram" class="icon" />
-        </a>
-        <a :href="artist.youtube" target="_blank">
-          <youtube-icon v-if="artist.youtube" class="icon" />
-        </a>
-        <a :href="artist.music" target="_blank">
-          <music-icon v-if="artist.music" class="icon" />
-        </a>
-        <a :href="artist.spotify" target="_blank">
-          <spotify-icon v-if="artist.spotify" class="icon" />
-        </a>
-        <p>
-          <span v-if="artist.quote" class="schedule-quote">
-            {{artist.quote}}
+      <div class="artists">
+        <div
+          v-for="(artist, i) in schedule"
+          @mouseover="hover = i"
+          :class="artist"
+          :key="`artist-${i}`"
+          :alt="artist"
+        >
+          <p>
+            <span class="schedule-artist">{{ schedule[i].artist }}</span>
             <br />
-          </span>
-          Members:
-          <span class="members">{{schedule[hover].members}}</span>
-          <br />
-          {{schedule[hover].bio}}
-          {{schedule[hover].bioB}}
-          {{schedule[hover].bioC}}
-        </p>
+            <span class="schedule-day">{{ schedule[i].day }}</span>/
+            <span class="schedule-time">{{ schedule[i].time }}</span>/
+            <span class="schedule-stage">{{ schedule[i].stage }}</span>
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -63,8 +30,94 @@ export default {
   data() {
     return {
       schedule,
-      hover: 0
+      hover: 3,
+      isActive: false
     };
   }
 };
 </script>
+
+<style scoped lang="scss">
+#background {
+  transition: all 0.5s ease;
+  transition-delay: 0.3s;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.poster-header {
+  //Positioning
+
+  //////////
+  //margin-bottom: 3rem;
+  width: 75vw;
+  height: auto;
+  fill: #eee;
+}
+
+h1 {
+  text-align: center;
+  font-size: 4rem;
+}
+
+.screen {
+  display: flex;
+  justify-content: center;
+  flex-flow: row wrap;
+  background-color: rgba(0, 0, 0, 0.4);
+  height: 100%;
+  width: 100%;
+  padding: 1rem;
+}
+
+.artists {
+  display: flex;
+  flex-flow: row wrap;
+}
+
+.artist {
+  margin-top: 1rem;
+  transition: all 0.5s ease;
+  flex: 1 0 auto;
+
+  &:hover {
+    transform: scale(1.1);
+    transform: translateX(2rem);
+  }
+}
+
+.schedule-artist {
+  font-size: 1.6rem;
+  text-transform: uppercase;
+  font-weight: 800;
+}
+
+//.schedule-day {}
+//.schedule-time {}
+
+.schedule-artist {
+  transform: translateX(2rem);
+}
+
+@media screen and (min-width: 40rem) {
+  .poster-header {
+    width: 50vw;
+  }
+
+  .screen {
+    padding: 4rem;
+  }
+  h1 {
+    font-size: 7rem;
+  }
+  .schedule-artist {
+    font-size: 2.5rem;
+  }
+  .artist {
+    &:hover {
+      transform: scale(1.15);
+    }
+  }
+}
+</style>
+
