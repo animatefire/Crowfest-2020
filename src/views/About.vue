37f1c17f727ed0c598c310a43062ff0881c1398f
John E. Pfannkuchen <johnpfannkuchen.webdev@gmail.com>
2020-03-19 15:03:01 -0400

    Fleshed out the about, camping, and location sections.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,29 +1,90 @@
 <template>
-  <div class="about">
-    <mincer class="icon" />
-    <p>The Audie J. Mincer PTSD/Cancer Awareness group is dedicated to helping those who suffer in silence from the unseen affliction known as PTSD and those who have been affected by cancer. The group came to life following the tragic loss of a family member, Audie J. Mincer, who was a Veteran and passed from cancer in 2014. It became a NYS registered not for profit corporation in 2016.</p>
+  <div id="about">
+    <p>
+      What’s the best part about bringing together a group of like-minded
+      individuals? Their drive to connect with one another and support local
+      causes. Crowfest is all about local music and bringing the community
+      together to help our neighbors in need.
+    </p>
 
-    <p>Summer Solstice Celebration CrowFest is held on Summer Solstice. Join us in celebrating the start of summer with a bonfire each night. Bring something to burn! Bring an instrument and join in on the pickin'. ​ Enjoy the fire dancing of lovely Karrah!</p>
+    <h3>Great Local Music</h3>
+    <p>
+      This year’s festival features an outstanding line-up of excellent local
+      musicians. There will be non-stop music with two stages to keep the music
+      going.
+    </p>
+
+    <h3>Great Local Food</h3>
+    <p>
+      Our festival features some of the best and most delectable local eats
+      around. Because after all, what's better than live music and amazing
+      food--all for a great cause? Nothing!
+    </p>
+
+    <h3>Fireside Jam</h3>
+    <p>
+      Both nights will end with a fireside jam--so bring something to burn and
+      an instrument to join in on the pickin' around the fire!
+    </p>
+
+    <h3>Family Friendly</h3>
+    <p>
+      Bring the WHOLE family! Crowfest is family and dog friendly. Bring
+      everyone along to join in the fun and support a great cause! There is a
+      playground and lots of space for your little ones (both 2 and 4-legged) to
+      enjoy at park. Just remember to bring a leash for your dog and a way to
+      scoop the poop :)
+    </p>
+
+    <h3>All Proceeds Go to Charity</h3>
+    <p>
+      ALL proceeds from the CrowFest will be donated to the Audie J. Mincer
+      Association to assist veterans suffering from PTSD and those affected by
+      cancer.
+    </p>
+
+    <mincer class="icon" />
+    <p>
+      The Audie J. Mincer PTSD/Cancer Awareness group is dedicated to helping
+      those who suffer in silence from the unseen affliction known as PTSD and
+      those who have been affected by cancer. The group came to life following
+      the tragic loss of a family member, Audie J. Mincer, who was a Veteran and
+      passed from cancer in 2014. It became a NYS registered not for profit
+      corporation in 2016.
+    </p>
+
+    <p>
+      Summer Solstice Celebration CrowFest is held on Summer Solstice. Join us
+      in celebrating the start of summer with a bonfire each night. Bring
+      something to burn! Bring an instrument and join in on the pickin'. ​ Enjoy
+      the fire dancing of lovely Karrah!
+    </p>
   </div>
 </template>
 
 <style scoped lang="scss">
-.about {
-  display: flex;
-  flex-flow: column nowrap;
-  align-content: center;
-}
+  #about {
+    padding: 1rem;
+    display: flex;
+    flex-flow: column nowrap;
+    align-content: center;
+  }
 
-.icon {
-  //min-width: 20rem;
+  h3 {
+    text-align: center;
+    font-weight: 800;
+  }
 
-  max-height: 30vh;
-  width: auto;
-  max-width: 100vw;
-}
+  .icon {
+    //min-width: 20rem;
 
-p {
-  padding: 1rem;
-  max-width: 80rem;
-}
+    max-height: 30vh;
+    width: auto;
+    max-width: 100vw;
+  }
+
+  p {
+    padding: 1rem;
+    max-width: 80rem;
+  }
 </style>
